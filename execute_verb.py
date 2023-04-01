49703f27927702a4f2c9cccdfcda98f6b1207bca
Ramanish Reddy Eeda <reeda@stevens.edu>
2023-04-01 06:12:44 +0000

    added get verb

--- execute_verb.py
+++ execute_verb.py
@@ -54,10 +54,58 @@ def execute_inventory(user_input, world_state):
 		print("  " + world_state.inventory[i])
 	return world_state
 
+def get_matched_item(user_input_item, current_room_items):
+    if user_input_item in current_room_items:
+        return user_input_item
+    filtered_items = list(filter(lambda word: re.search(user_input_item, sanitize_string(word)), current_room_items))
+    if len(filtered_items) == 1:
+        return filtered_items[0]
+    elif len(filtered_items) > 1:
+        output_str = "Did you want to get"
+        for i in range(0,len(filtered_items),1):
+            if i == 0:
+                output_str += " the"
+            elif i == len(filtered_items) - 1:
+                output_str += " or the"
+            else:
+                output_str += ","
+            output_str += " " + filtered_items[i]
+        output_str += "?"
+        print(output_str)
+        return 0
+    else:
+        return -1
+
+def execute_get(user_input, world_state):
+    n = len(user_input)
+    if n == 1:
+        print("Sorry, you need to 'get' something.")
+        return world_state
+
+    user_input_item = user_input[1]
+    if "items" not in world_state.room_map[world_state.current_room_index] : 
+        current_room_items = []
+    else :
+        current_room_items = world_state.room_map[world_state.current_room_index]["items"]
+    matched_item = get_matched_item(user_input_item, current_room_items)
+
+    if matched_item == 0:
+        return world_state
+    elif matched_item == -1:
+        print("There's no " + user_input_item + " anywhere.")
+        return world_state
+    else:
+        user_input_item = matched_item
+  
+    print("You pick up the " + user_input_item + ".")
+    world_state.room_map[world_state.current_room_index]["items"].remove(user_input_item)
+    world_state.inventory.insert(0, user_input_item)
+    return world_state
+
 	
 def execute_user_input(user_input, world_state):
 	verb = user_input[0]
-	verbs = ["go", "look", "inventory"]
+	verbs = ["go", "look", "inventory","get"]
 	if verb in verbs:
 		verb_found = verb
 	
@@ -67,3 +115,5 @@ def execute_user_input(user_input, world_state):
 		return execute_look(user_input, world_state)
 	elif verb_found == "inventory":
 		return execute_inventory(user_input, world_state)
+	elif verb_found == "get":
+		return execute_get(user_input, world_state)
